5e2ef271a590c5ab5a65547018d40daf25b171f3
Poi <user@seznam.cz>
2022-04-24 12:47:48 +0200

    ...

--- src/main/java/org/rk/entity/Customer.java
+++ src/main/java/org/rk/entity/Customer.java
@@ -1,16 +1,10 @@
 package org.rk.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 import java.time.LocalDate;
 
 @Data
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor                      // zadny
 
 public class Customer {
     private int id;
